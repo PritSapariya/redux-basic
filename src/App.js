8b7038a1c76fcbf0ad2a83b0e8b093e93096ced8
PritSapariya <pritsapariya17@gmail.com>
2021-04-15 20:41:28 +0530

    Complete basic code for React Redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'; 
+import { decrement, increment } from './actions';
 
 const App = () => {
+
+    const counter = useSelector(state => state.counterReducer);
+    const isLogged = useSelector(state => state.loggedReducer);
+    const dispatch = useDispatch();
+
     return (
-        <>Hello world!</>
+        <div>
+            <button onClick={() => dispatch(increment(5))}>+</button>
+            {counter}
+            <button onClick={() => dispatch(decrement(5))}>-</button>
+            {isLogged ? <p>Valuable information should not see</p> : null}
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import ReactDom from 'react-dom';
 import App from './App.js';
 import { createStore } from 'redux';
 import allReducers from './reducers/index.js'
+import { Provider } from 'react-redux';
+ 
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-const store = createStore(allReducers);
 
-ReactDom.render(<App />, document.getElementById('root'));
+
+ReactDom.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'));
