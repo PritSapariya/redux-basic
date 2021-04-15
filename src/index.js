bd62dcd417052d56a128658f7151ca7226478292
PritSapariya <pritsapariya17@gmail.com>
2021-04-15 18:13:14 +0530

    Underconstruction code

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import App from './App.js';
+import { createStore } from 'redux';
+import allReducers from './reducers/index.js'
+
+const store = createStore(allReducers);
 
 ReactDom.render(<App />, document.getElementById('root'));
